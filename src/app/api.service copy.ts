484eac6f98fa9686adeeaceab6933bad831e6223
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-09-04 13:12:18 +0500

    update

--- src/app/api.service copy.ts
+++ src/app/api.service copy.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {environment} from '../environments/environment.prod'
+import {environment} from './environments/environment.prod'
 import { Authservice } from './services/authservice';
 
 
