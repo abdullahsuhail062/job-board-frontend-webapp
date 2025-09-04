e7ebdf31b1cd7fbdb482c1749c53f982227c9746
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-09-04 14:14:29 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RedirectCommand, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardService } from './service/auth-guard.service';
+import { Authservice } from './services/authservice';
+import { AuthGuardServiceService } from './auth-guard-service-service';
+import { AdminNotificationComponent } from './admin-notification/admin-notification.component';
+import { AdminGuard } from './guards/admin.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [{path: '', component: LandingComponent,canActivate: [AuthGuardService]}, {path: 'signup', component: SignupComponent, canActivate: [AuthGuardService]}, {path: 'signin', component: SigninComponent, canActivate: [AuthGuardService]},{path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardServiceService]},{path: 'admin-notification', component: AdminNotificationComponent, canActivate: [AdminGuard]}];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Authservice } from './services/authservice';
+import { DarkModeService } from './dark-mode.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('job-board-frontendApp');
+   constructor(private darkModeService: DarkModeService ,private authService: Authservice, private router: Router){}
+
+ngOnInit(): void {
+  // if (this.darkModeService.getDarkMode()) {
+  //   this.darkModeService.enableDarkMode()
+    
+  // }else this.darkModeService.disableDarkMode()
+  }
 }
